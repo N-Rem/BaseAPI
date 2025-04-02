dc0d252beb7335a7a26331948ded348d38ac414d
Nico <nicolasromero.barrios@gmail.com>
2025-04-02 17:14:21 -0300

    add initial Infraestrcutre

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Son las instrucciones de mapeo del ORM
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //dbset recive una entidad y permite traducirlo a una tabla y la tabla con ese nombre se traduce a entidad
+       public DbSet<User> Users { get; set; }
+       public DbSet<Project> Projects { get; set; }
+       public DbSet<UserProject> UserProjects { get; set; }
+       public DbSet<Tool> Tools { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //relacion de las tools con el user
+            modelBuilder.Entity<Tool>()
+                .HasOne<User>() //Tool tiene una relación con User
+                .WithMany()
+                .HasForeignKey(t => t.UserTool);
+
+            //relaciones entre user y prject
+            modelBuilder.Entity<UserProject>()
+                .HasOne<Project>()
+                .WithMany()
+                .HasForeignKey(p => p.ProjectId);
+            //.OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserProject>()
+                .HasOne<User>()
+                .WithMany()
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+}
+
---
+++ Infrastructure/Data/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+        //Clase genérica, repositorio base, T es como un comodin que nos permite usarlo en todas las entidades. 
+        //Esta clase no existe hasta el momento en que se la llama a travez de otro repositorio. 
+     public class BaseRepository <T> : IBaseRepository<T> where T : class
+    {
+        
+        // Contexto de base de datos de Entity Framework
+        private readonly DbContext _dbContext;
+        public BaseRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync<TId>(TId id)
+        {
+            return await _dbContext.Set<T>().FindAsync(new object[] { id });
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
