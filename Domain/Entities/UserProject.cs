30f0dfb968e54f00a38833315228044dcb74de18
Nico <nicolasromero.barrios@gmail.com>
2025-04-02 14:08:48 -0300

    add initial domain

---
+++ Domain/Entities/UserProject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UserProject
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int UserId {get; set; }
+        public int ProjecId { get; set; } 
+    }
+}
