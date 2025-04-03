f4193789074f18348a723ff24f2b921f7a2463cd
Nico <nicolasromero.barrios@gmail.com>
2025-04-03 16:35:02 -0300

    add migrations and AplicationBase

---
+++ Application/Services/UserServices.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class UserServices
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserServices(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+    }
+}
--- Domain/Entities/Project.cs
+++ Domain/Entities/Project.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Enums;
 
 namespace Domain.Entities
 {
@@ -16,6 +17,8 @@ namespace Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public Status Status { get; set; } = Status.Active;
 
     }
 }
