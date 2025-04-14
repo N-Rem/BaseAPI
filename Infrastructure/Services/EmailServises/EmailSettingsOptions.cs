21f20a3a56ac0a5130f94a97ea40195dece4d0c3
Nico <nicolasromero.barrios@gmail.com>
2025-04-14 14:17:55 -0300

    Fixes enviroment variable

--- Infrastructure/Services/EmailServises/EmailSettingsOptions.cs
+++ Infrastructure/Services/EmailServises/EmailSettingsOptions.cs
@@ -13,12 +13,12 @@ namespace Infrastructure.Services.EmailServises
         public int Port { get; set; } 
         public string UserName { get; set; }
 
-        public string Password { get; set; } = string.Empty;
-        //public string Password
-        //{
-        //    get => Environment.GetEnvironmentVariable("MAIL_PASSWORD") ?? _password;
-        //    set => _password = value;
-        //}
-        //private string _password;
+        //public string Password { get; set; } = string.Empty;
+        public string Password
+        {
+            get => Environment.GetEnvironmentVariable("MAIL_PASSWORD") ?? _password;
+            set => _password = value;
+        }
+        private string _password;
     }
 }
